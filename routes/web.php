95bb623d473b7721a9b68183e4558cc7b1974e1c
LegacyVault <legacyvault14@gmail.com>
2025-09-01 13:43:56 +0800

    Cleanout BE Files

--- routes/web.php
+++ routes/web.php
@@ -3,45 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('products', function() {
-        return Inertia::render('products/index');
-    })->name('products');
-});
-
-//add product
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('add-product', function() {
-        return Inertia::render('products/add-product');
-    })->name('add-product');
-});
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('add-product/{id?}', function ($id = null) {
-        $product = $id ? 'edit' : null; //temporary solution
-
-        //enable when there's get product api
-        // if ($id) {
-        //     $product = Product::findOrFail($id); // preload product if editing
-        // }
-
-        return Inertia::render('products/add-product', [
-            'product' => $product,
-        ]);
-
-    })->name('add-product');
-});
-
-
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+#WEB.PHP Currently Not Used
