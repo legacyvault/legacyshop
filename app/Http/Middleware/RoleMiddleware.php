f8a950504f80bd81dc0fd9272664a9ced9ac8ed5
LegacyVault <legacyvault14@gmail.com>
2025-11-06 16:27:44 +0800

    Order Redirect Notification Hit API

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -12,8 +12,7 @@ class RoleMiddleware
     public function handle(Request $request, Closure $next, string $role)
     {
         $user = Auth::user();
-        Log::info('Role Middleware Start..');
-        Log::info($user);
+
         if (!$user) {
             Auth::logout();
             return redirect()->route('login')->with('error', 'You are not allowed.');
