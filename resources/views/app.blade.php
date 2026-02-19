c3e61180312e4a30499c020847ab273919b5490c
Gifino Thoriq <gifinothoriq@Gifinos-MacBook-Air.local>
2026-02-20 04:35:40 +0700

    chore: change logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -40,7 +40,7 @@
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <link rel="icon" href="/logo.ico" sizes="any">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="apple-touch-icon" href="/logo.png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
