313ceaf79f6990676c1257053bd17e0f2629954f
LegacyVault <legacyvault14@gmail.com>
2026-04-19 21:38:05 +0800

    Fix email

--- app/Mail/OrderConfirmedMail.php
+++ app/Mail/OrderConfirmedMail.php
@@ -27,7 +27,7 @@ class OrderConfirmedMail extends Mailable
     public function build()
     {
         return $this->subject("ORDER CONFIRMED - {$this->orderId}")
-            ->text('emails.order_confirmed_plain');
+            ->view('emails.order_confirmed_plain');
     }
 
     /**
@@ -46,7 +46,7 @@ class OrderConfirmedMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            text: 'emails.order_confirmed_plain',
+            view: 'emails.order_confirmed_plain',
         );
     }
 
